622b8300816401db7278895899591edf531eb982
Your Name <xkacperer@gmail.com>
2026-05-02 23:43:39 +0200

    made her

--- index.js
+++ index.js
@@ -112,7 +112,6 @@ client.on(Events.MessageCreate, async message => {
     } catch (e) { console.error(e); }
 });
 // Inside client.on(Events.MessageCreate...)
-const safeUserInput = `"""${message.content}"""`;
 app.get('/', (req, res) => res.send('Bot is awake and secure! 🛡️'));
 const port = process.env.PORT || 10000;
 app.listen(port, '0.0.0.0', () => {
